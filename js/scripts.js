ae7a54d271cff31703169f95f48f62aa92e73887
shresthapr <69511034+shresthapr@users.noreply.github.com>
2020-08-14 12:44:50 +0300

    branches addition

---
+++ js/scripts.js
@@ -0,0 +1,58 @@
+<!DOCTYPE html><html prefix="og: http://ogp.me/ns fb: http://www.facebook.com/2008/fbml">
+<!-- refreshed Fri Aug 14 11:09:15 CEST 2020 -->
+<head>
+<link rel="dns-prefetch" href="//elkjop.tt.omtrdc.net/">
+<link rel="dns-prefetch" href="//cdn.tt.omtrdc.net/">
+<link rel="dns-prefetch" href="//display.ugc.bazaarvoice.com/">
+<link rel="dns-prefetch" href="//elkjop.112.2o7.net/">
+<link rel="dns-prefetch" href="//smetrics.elkjop.no/">
+<link rel="dns-prefetch" href="//metrics.elkjop.no/">
+<link rel="dns-prefetch" href="//dpm.demdex.net/">
+<link rel="dns-prefetch" href="//cdn-eu.dynamicyield.com">
+<link rel="dns-prefetch" href="//st-eu.dynamicyield.com">
+<link rel="dns-prefetch" href="//rcom-eu.dynamicyield.com">
+<link rel="dns-prefetch" href="//assets.adobedtm.com/">
+<link rel="dns-prefetch" href="/eum.instana.io" />
+<link rel="dns-prefetch preconnect" href="//static.queue-it.net/" crossorigin />
+<link rel="dns-prefetch preconnect" href="//assets.queue-it.net/" crossorigin />
+<link rel="dns-prefetch preconnect" href="//apps.bazaarvoice.com/" crossorigin />
+<link rel="dns-prefetch preconnect" href="//apps.nexus.bazaarvoice.com" crossorigin />
+<link rel="dns-prefetch preconnect" href="//analytics-static.ugc.bazaarvoice.com" crossorigin />
+<link rel="dns-prefetch preconnect" href="//static.criteo.net" crossorigin />
+<link rel="dns-prefetch preconnect" href="//script.hotjar.com" crossorigin />
+<link rel="dns-prefetch preconnect" href="//async-px-eu.dynamicyield.com" crossorigin />
+<script id="CookieConsent" src="https://policy.app.cookieinformation.com/uc.js" data-culture="FI" type="text/javascript"></script>
+<script type="text/javascript">
+function displayCookieBlockingText(cookieCategory, elementSelector, selectorType){
+if(!CookieInformation.getConsentGivenFor(cookieCategory)){
+cLinkATag = document.createElement("a");
+cLinkATag.href = 'javascript:CookieConsent.renew()';
+cLinkATag.innerHTML = 'Klikkaa t&auml;st&auml;';
+if(selectorType === 'id'){
+cBlockElement = document.getElementById(elementSelector);
+}else{
+cBlockElement = document.querySelector("."+elementSelector); 
+}
+cBlockElement.querySelector('.el-cookies-blocked') ? cBlockElement.querySelector('.el-cookies-blocked').remove() : ''
+cDivTag = document.createElement("div");
+cDivTag.classList.add('el-cookies-blocked');
+cDivTag.innerHTML = 'Klikkaa t&auml;st&auml;';
+if(cookieCategory === 'functional'){
+cDivTag.innerHTML = "T&auml;m&auml;n sivun komponenttia ei voida n&auml;ytt&auml;&auml;, koska se k&auml;ytt&auml;&auml; toiminnallisia ev&auml;steit&auml;, joita et ole hyv&auml;ksynyt. ";
+}else if(cookieCategory === 'statistical'){
+cDivTag.innerHTML = "T&auml;m&auml;n sivun komponenttia ei voida n&auml;ytt&auml;&auml;, koska se k&auml;ytt&auml;&auml; tilastollisia ev&auml;steit&auml;, joita et ole hyv&auml;ksynyt. ";
+}else if(cookieCategory === 'marketing'){
+cDivTag.innerHTML = "T&auml;m&auml;n sivun komponenttia ei voida n&auml;ytt&auml;&auml;, koska se k&auml;ytt&auml;&auml; markkinointiev&auml;steit&auml;, joita et ole hyv&auml;ksynyt. ";
+}else if(cookieCategory === 'unclassified'){
+cDivTag.innerHTML = "T&auml;m&auml;n sivun komponenttia ei voida n&auml;ytt&auml;&auml;, koska se k&auml;ytt&auml;&auml; luokittelemattomia ev&auml;steit&auml;, joita et ole hyv&auml;ksynyt. ";
+}else{
+cDivTag.innerHTML = "T&auml;m&auml;n sivun komponenttia ei voida n&auml;ytt&auml;&auml;, koska se k&auml;ytt&auml;&auml; ev&auml;steit&auml;, joita et ole hyv&auml;ksynyt. ";
+}
+cDivTag.appendChild(cLinkATag); 
+cBlockElement.innerHTML = '';
+cBlockElement.appendChild(cDivTag);
+}
+}
+</script>
+
+ 
